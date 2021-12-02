7deae186644999101709e3b45d298328780b8735
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2021-12-02 10:06:35 -0600

    simplify how we inject sprite for tests
    Use existing test-body-footer instead of creating a custom one

--- ember-flight-icons/index.js
+++ ember-flight-icons/index.js
@@ -11,7 +11,7 @@ module.exports = {
     // otherwise the @percy/ember package ignores everything else and the SVG sprite is not added to the DOM
     // see thread: https://hashicorp.slack.com/archives/C11JCBJTW/p1633978558343000
     // see: https://github.com/percy/percy-ember/blob/master/addon-test-support/%40percy/ember/index.js#L33
-    if (type === 'body-footer' || type === 'ember-testing-sprite-embed') {
+    if (type === 'body-footer' || type === 'test-body-footer') {
       return flightIconSprite;
     }
   },
